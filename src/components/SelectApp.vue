dd40086702baa3152bc2bbca35beaf9124af653d
daniel cruz <dannyfsociety@gmail.com>
2018-05-09 03:04:36 -0400

    Select Module Almost Done

--- src/components/SelectApp.vue
+++ src/components/SelectApp.vue
@@ -1,80 +1,68 @@
 <template>
 <div>
-         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-            <select class="mdl-textfield__input" id="faculty" v-model="getFacultyId" v-on:change="filterCareersByFaculty()">
-              <option disabled>Elige Una Facultad</option>
-              <option v-for="faculty in faculties" :key="faculty.id" :value="faculty.id">{{faculty.name}}</option>
-            </select>
-            <label class="mdl-textfield__label pd-10-ml" for="faculty">Facultad</label>
-          </div>
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+    <select class="mdl-textfield__input" id="faculty" v-model="getFacultyId" v-on:change="filterCareersByFaculty()">
+      <option disabled>Elige Una Facultad</option>
+      <option v-for="faculty in faculties" :key="faculty.id" :value="faculty.id">{{faculty.name}}</option>
+    </select>
+    <label class="mdl-textfield__label pd-10-ml" for="faculty">Facultad</label>
+  </div>
 
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-            <select class="mdl-textfield__input" id="career" name="career">
-              <!-- <option selected>xxxxx</option> -->
-              <option></option>
-              <option v-for="career in careers" :key="career.id">{{career.name}}</option>
-            </select>
-            <label class="mdl-textfield__label pd-10-ml" for="career">Carrera</label>
-          </div>
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+    <select class="mdl-textfield__input" id="career" v-model="getCareerId" v-on:change="filterSubjectsByCareer()">
+      <option></option>
+      <option v-for="career in careers" :key="career.id" :value="career.id">{{career.name}}</option>
+    </select>
+    <label class="mdl-textfield__label pd-10-ml" for="career">Carrera</label>
+  </div>
 
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-            <select class="mdl-textfield__input" id="subject" name="subject">
-              <option></option>
-              <!-- <option v-for="faculty in faculties" :key="faculty.id">{{faculty.name}}</option> -->
-              <option value="87">Calculo 1</option>
-              <option value="89">Calculo 2</option>
-              <option value="91">Redes</option>
-            </select>
-            <label class="mdl-textfield__label pd-10-ml" for="subject">Asignatura</label>
-          </div>
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+    <select class="mdl-textfield__input" id="subject" v-model="getSubjectId" v-on:change="filterParallelsBySubjectAndCareer()">
+      <option></option>
+      <option v-for="subject in subjects" :key="subject.subject_id" :value="subject.subject_id">{{subject.name}}</option>
+    </select>
+    <label class="mdl-textfield__label pd-10-ml" for="subject">Asignatura</label>
+  </div>
 
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-            <select class="mdl-textfield__input" id="subject" name="subject">
-              <option></option>
-              <!-- <option v-for="faculty in faculties" :key="faculty.id">{{faculty.name}}</option> -->
-              <option value="87">Paralelo 1</option>
-              <option value="89">Paralelo 2</option>
-              <option value="91">Paralelo 3</option>
-            </select>
-            <label class="mdl-textfield__label pd-10-ml" for="parallel">Paralelo</label>
-          </div>
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+    <select class="mdl-textfield__input" id="subject" name="subject" v-model="getParallelId" v-on:change="filterProfessorsBySubjectAndParallel()">
+      <option></option>
+      <option v-for="parallel in parallels" :key="parallel.paralleel_id" :value="parallel.parallel_id">{{parallel.detail}}</option>
+    </select>
+    <label class="mdl-textfield__label pd-10-ml" for="parallel">Paralelo</label>
+  </div>
 
 
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-            <select class="mdl-textfield__input" id="professor" name="professor">
-              <option></option>
-              <!-- <option v-for="faculty in faculties" :key="faculty.id">{{faculty.name}}</option> -->
-              <option value="87">Jhon Smith</option>
-              <option value="89">Alberto Bravo</option>
-              <option value="91">Yiang Yen</option>
-            </select>
-            <label class="mdl-textfield__label pd-10-ml" for="professor">Docente</label>
-          </div>
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+    <select class="mdl-textfield__input" id="professor" v-model="getProfessorId">
+      <option></option>
+      <option v-for="professor in professors" :key="professor.professor_id" :value="professor.professor_id">
+        {{professor.name}} {{professor.lastname_p}} {{professor.lastname_s}}
+      </option>
+    </select>
+    <label class="mdl-textfield__label pd-10-ml" for="professor">Docente</label>
+  </div>
 
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-            <select class="mdl-textfield__input" id="professor" name="professor">
-              <option></option>
-              <!-- <option v-for="faculty in faculties" :key="faculty.id">{{faculty.name}}</option> -->
-              <option value="87">1/2018</option>
-              <option value="89">2/2018</option>
-              <option value="91">3/2018</option>
-            </select>
-            <label class="mdl-textfield__label pd-10-ml" for="professor">Gestion</label>
-          </div>
+  <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+    <select class="mdl-textfield__input" id="management" v-model="getManagementId">
+      <option></option>
+      <option v-for="management in managements" :key="management.id" :value="management.id">
+         {{management.number}}/{{management.year}}
+      </option>
+    </select>
+    <label class="mdl-textfield__label pd-10-ml" for="professor">Gestion</label>
+  </div>
 
-          <ul class="demo-list-control mdl-list pd-10-ml">
-            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
-                <input class="mdl-textfield__input" type="text" id="fecha" disabled="" value="10-04-2018">
-                <label class="mdl-textfield__label" for="fecha" style="color: #3da6dd;">Fecha</label>
-              </div>
-          </ul>
+  <ul class="demo-list-control mdl-list pd-10-ml">
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label width-100">
+        <input class="mdl-textfield__input" type="text" id="fecha" disabled="" v-model="currentDate">
+        <label class="mdl-textfield__label" for="fecha" style="color: #3da6dd;">Fecha</label>
+      </div>
+  </ul>
 
-          
-  
-
-         <div class="center pd-bm-20">
-            <router-link to="/form/check" class="btn-ml-submit blue-ml">Aceptar</router-link>
-         </div>
+  <div class="center pd-bm-20">
+    <router-link to="/form/check" class="btn-ml-submit blue-ml">Aceptar</router-link>
+  </div>
 </div>
 </template>
 
@@ -84,11 +72,24 @@ export default {
       return{
          faculties: [],
          careers: [],
-         getFacultyId: ''
+         subjects: [],
+         parallels: [],
+         professors: [],
+         managements: [],
+         getFacultyId: '',
+         getCareerId: '',
+         getSubjectId: '',
+         getParallelId: '',
+         getProfessorId: '',
+         getManagementId: '',
+         currentDate: new Date().toLocaleString()
       }
    },
    created () {
        this.$http.get('api/faculties').then(response => this.faculties = response.body)
+       .catch(error => console.log(error.response.body));
+
+       this.$http.get('api/managements').then(response => this.managements = response.body)
        .catch(error => console.log(error.response.body));
    },
    methods: {
@@ -96,6 +97,20 @@ export default {
            this.$http.get('api/careers/'+ this.getFacultyId).then(response => {
                this.careers = response.body;
            }).catch(error => console.log(error.response.body));
+       },
+       filterSubjectsByCareer(){
+           this.$http.get('api/subjects/'+ this.getCareerId).then(response => this.subjects = response.body)
+           .catch(error => console.log(error.response.body));
+       },
+       filterParallelsBySubjectAndCareer(){
+           this.$http.get('api/parallels/'+ this.getCareerId + '/' + this.getSubjectId)
+           .then(response => this.parallels = response.body)
+           .catch(error => console.log(error.response.body));
+       },
+       filterProfessorsBySubjectAndParallel(){
+           this.$http.get('api/professors/'+ this.getSubjectId + '/' + this.getParallelId)
+           .then(response => this.professors = response.body)
+           .catch(error => console.log(error.response.body));
        }
    }
 }
